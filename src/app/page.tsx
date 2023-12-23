821520d3d0e4fa6e09989a82ad97d51044324d6a
Eduardo Picazo <edy7picazo@gmail.com>
2023-12-22 22:37:20 -0600

    feat: remove dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import db from "@/db"
 import Link from "next/link";
 
-export const dynamic = 'force-dynamic';
+// Force dynamic rendering
+//export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
